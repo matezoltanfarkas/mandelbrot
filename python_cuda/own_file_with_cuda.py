748378ecae956cd6f8abf7aca053b1e74e2fb9e7
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2024-08-21 23:00:39 +0200

    Adding most recent version (~22:30 wed evening) of python numba cuda

--- python_cuda/own_file_with_cuda.py
+++ python_cuda/own_file_with_cuda.py
@@ -1,13 +1,18 @@
 import cupy as cp
-import matplotlib.pyplot as plt
 import numba as nb
-import numpy as np
 from numba import cuda
+from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32
+import numpy as np
 
-from utils import plot_pixels
+from utils import (
+    plot_pixels,
+    combine_uncertaintes,
+    confidence_interval
+)
 
-NUM_TILES_1D = 100
-SAMPLES_IN_BATCH = 100  # Define the constant
+import matplotlib.pyplot as plt
+
+NUM_TILES_1D = 64
 
 @cuda.jit(device=True)
 def wald_uncertainty(numer, denom):
@@ -36,27 +41,18 @@ def is_in_mandelbrot(x, y):
         if z_hare.real**2 + z_hare.imag**2 > 4:
             return False  # diverging to infinity
 
-@cuda.jit(device=True)
-def count_mandelbrot(rng, num_samples, xmin, width, ymin, height):
-    """Draw num_samples random numbers uniformly between (xmin, xmin+width)
-    and (ymin, ymin+height)."""
-    count = 0
-    for idx in range(num_samples):
-        x_norm = rng[idx, 0]
-        y_norm = rng[idx, 1]
-        x = xmin + (x_norm * width)
-        y = ymin + (y_norm * height)
-        count += is_in_mandelbrot(x, y)
-    return count
 
 @cuda.jit
-def compute_until(rngs, numer, denom, uncert, uncert_target, width, height, num_samples, num_tiles):
+def compute_until(rng_states, numer, denom, uncert, uncert_target, width, height, num_tiles):
     """Compute area of each tile until uncert_target is reached.
     The uncertainty is calculated with the Wald approximation in each tile.
     """
+    
     i, j = cuda.grid(2)
+    num_samples = 100
+
     if i < num_tiles and j < num_tiles:
-        rng = rngs[num_tiles * i + j]
+        
         uncert[i, j] = float('inf')
 
         xmin_val = -2 + width * j
@@ -64,20 +60,26 @@ def compute_until(rngs, numer, denom, uncert, uncert_target, width, height, num_
 
         while uncert[i, j] > uncert_target:
             denom[i, j] += num_samples
-            count = count_mandelbrot(rng, num_samples, xmin_val, width, ymin_val, height)
-            numer[i, j] += count
+            
+            for _ in range(num_samples):
+                x_norm = xoroshiro128p_uniform_float32(rng_states, i+j)
+                y_norm = xoroshiro128p_uniform_float32(rng_states, i+j)
+                x = xmin_val + (x_norm * width)
+                y = ymin_val + (y_norm * height)
+                if is_in_mandelbrot(x, y):
+                    numer[i, j] += 1
 
             uncert[i, j] = wald_uncertainty(numer[i, j], denom[i, j]) * width * height
 
-# CuPy RNG setup and conversion to CuPy arrays
-rngs = cp.random.random((NUM_TILES_1D * NUM_TILES_1D, SAMPLES_IN_BATCH, 2), dtype=cp.float32)
+
+rng_states = create_xoroshiro128p_states(32*32 * 1, seed=11)
 
 xmin, xmax = -2, 1
 ymin, ymax = -3 / 2, 3 / 2
 
 width = 3.0 / NUM_TILES_1D
 height = 3.0 / NUM_TILES_1D
-uncert_target = 1e-5
+uncert_target = 1e-4
 
 # Allocate GPU memory
 numer = cp.zeros((NUM_TILES_1D, NUM_TILES_1D), dtype=cp.float32)
@@ -85,17 +87,26 @@ denom = cp.zeros((NUM_TILES_1D, NUM_TILES_1D), dtype=cp.float32)
 uncert = cp.zeros((NUM_TILES_1D, NUM_TILES_1D), dtype=cp.float32)
 
 # Define block and grid dimensions
-threads_per_block = (16, 16)
-blocks_per_grid = ((NUM_TILES_1D + threads_per_block[0] - 1) // threads_per_block[0],
-                   (NUM_TILES_1D + threads_per_block[1] - 1) // threads_per_block[1])
+threads_per_block = (32, 32)
+# Calculate how many blocks per grid are needed to cover
+blocks_per_grid_x = (NUM_TILES_1D + threads_per_block[0] - 1) // threads_per_block[0]
+blocks_per_grid_y = (NUM_TILES_1D + threads_per_block[1] - 1) // threads_per_block[1]
+
+blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
+
+print(blocks_per_grid)
 
 # Run the CUDA kernel
-compute_until[blocks_per_grid, threads_per_block](rngs, numer, denom, uncert, uncert_target, width, height, SAMPLES_IN_BATCH, NUM_TILES_1D)
+compute_until[blocks_per_grid, threads_per_block](rng_states, numer, denom, uncert, uncert_target, width, height, NUM_TILES_1D)
+
+nb.cuda.synchronize()
 
 # Copy the results back to the host if needed
 numer_host = cp.asnumpy(numer)
 denom_host = cp.asnumpy(denom)
 uncert_host = cp.asnumpy(uncert)
+
+print(np.unique(denom_host))
 
 final_value = (np.sum((numer_host / denom_host)) * width * height).item()
 print(final_value)
@@ -104,3 +115,16 @@ print(final_value)
 fig, ax, p = plot_pixels(numer_host / denom_host, dpi=80)
 fig.colorbar(p, ax=ax, shrink=0.8, label="fraction of sampled points in Mandelbrot set in each tile")
 plt.savefig('my_figure.pdf')
+
+
+CONFIDENCE_LEVEL = 0.05
+
+confidence_interval_low, confidence_interval_high = confidence_interval(
+    CONFIDENCE_LEVEL, numer_host, denom_host, width * height
+)
+
+final_uncertainty = combine_uncertaintes(
+    confidence_interval_low, confidence_interval_high, denom_host
+)
+print(final_uncertainty)
+print(f"Relative Uncertainty: {final_uncertainty/final_value}")
