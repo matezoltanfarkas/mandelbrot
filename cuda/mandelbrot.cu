32880c652aeef668f462ec9251783b6b270fb061
Máté Zoltán Farkas <mate.farkas@rwth-aachen.de>
2024-08-22 21:03:30 +0200

    it runs on a GPU

--- cuda/mandelbrot.cu
+++ cuda/mandelbrot.cu
@@ -6,39 +6,36 @@
 #include <chrono>
 #include <cstdio>
 #include <random>
+#include <curand_kernel.h>
+#include <cuComplex.h>
 
 using namespace std;
 
-const int NUM_TILES_1D = 100;
-const int SAMPLES_IN_BATCH = 100;
-const double width = 3. / NUM_TILES_1D;
-const double height = 3. / NUM_TILES_1D;
+const int NUM_TILES_1D = 10;
+const int SAMPLES_IN_BATCH = 10000;
+const float width = 3. / NUM_TILES_1D;
+const float height = 3. / NUM_TILES_1D;
 
-// Precision for double comparison in AreSame
-const double EPSILON = 1e-9;
+// Precision for float comparison i9n AreSame
+const float EPSILON = 1e-9;
 
 // ============ TOOLS ================
-__global__ void hello_world_gpu()
-{
-  printf("Hello World from the GPU at block %u, thread %u \n", blockIdx.x, threadIdx.x);
-}
-
-bool AreSame(double a, double b)
+bool AreSame(float a, float b)
 {
   return fabs(a - b) < EPSILON;
 }
 
-double xmin(int j)
+float xmin(int j)
 {
   return -2 + width * j;
 }
 
-double ymin(int i)
+float ymin(int i)
 {
   return -3 / 2 + height * i;
 }
 
-double wald_uncertainty(double numer, double denom)
+float wald_uncertainty(float numer, float denom)
 {
   if (AreSame(numer, 0.))
   {
@@ -50,13 +47,13 @@ double wald_uncertainty(double numer, double denom)
     denom++;
   }
 
-  double frac = numer / denom;
+  float frac = numer / denom;
   return sqrt(frac * (1.0 - frac) / denom);
 }
 
 // ============ MANDELBROT STUFF ================
 
-bool is_in_mandelbrot(const double x, const double y)
+bool is_in_mandelbrot(const float x, const float y)
 {
   // Tortoise and Hare approach to check if point (x, y) is in Mandelbrot set.
   std::complex<double> z_hare(0.0, 0.0);
@@ -79,19 +76,19 @@ bool is_in_mandelbrot(const double x, const double y)
   }
 }
 
-double count_mandelbrot(mt19937 rng, int num_samples, double x_min, double width, double y_min, double height)
+float count_mandelbrot(mt19937 rng, int num_samples, float x_min, float width, float y_min, float height)
 {
-  double out = 0.;
+  float out = 0.;
 
   // Random number generator distribution
   std::uniform_real_distribution<double> dist(0.0f, 1.0f); // Uniform distribution between 0 and 1
 
   for (int i = 0; i < num_samples; ++i)
   {
-    double x_norm = dist(rng);
-    double y_norm = dist(rng);
-    double x = x_min + (x_norm * width);
-    double y = y_min + (y_norm * height);
+    float x_norm = dist(rng);
+    float y_norm = dist(rng);
+    float x = x_min + (x_norm * width);
+    float y = y_min + (y_norm * height);
 
     if (is_in_mandelbrot(x, y))
     {
@@ -101,20 +98,100 @@ double count_mandelbrot(mt19937 rng, int num_samples, double x_min, double width
   return out;
 }
 
-void compute_until(vector<mt19937> &random_generators, vector<vector<double>> &numer, vector<vector<double>> &denom, vector<vector<double>> &uncert, double uncert_target)
+// void compute_until(mt19937 *random_generators, float *numer, float *denom, float *uncert, float uncert_target, curandState *d_state)
+__global__ void compute_until(float *numer, float *denom, float *uncert, float uncert_target, curandState *d_state)
 {
-  for (int i = 0; i < NUM_TILES_1D; i++)
-    for (int j = 0; j < NUM_TILES_1D; j++)
+  // TODO this!
+  // int blockI = blockIdx.x;
+  // int blockJ = blockIdx.y;
+  // int i = threadIdx.x;
+  // int j = threadIdx.y;
+  int i = threadIdx.x;
+  int j = blockIdx.x;
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  // vector<mt19937> &random_generators;
+  // for (int i = 0; i < NUM_TILES_1D; i++)
+  //   for (int j = 0; j < NUM_TILES_1D; j++)
+  //   {
+  // mt19937 rng = random_generators[NUM_TILES_1D * i + j];
+  uncert[index] = INFINITY;
+  while (uncert[index] > uncert_target)
+  {
+    denom[index] += (float)SAMPLES_IN_BATCH;
+    // begin copied code ---------------------=======================
+    float out = 0.;
+    for (int i2 = 0; i2 < SAMPLES_IN_BATCH; ++i2)
     {
-      mt19937 rng = random_generators[NUM_TILES_1D * i + j];
-      uncert[i][j] = numeric_limits<double>::infinity();
-      while (uncert[i][j] > uncert_target)
+      float x_norm = curand_uniform_double(d_state);
+      float y_norm = curand_uniform_double(d_state);
+      // printf("norms %lf %lf", x_norm, y_norm);
+      float x = -2 + width * j + (x_norm * width);
+      float y = -3 / 2 + height * i + (y_norm * height);
+
+      // copied code begin ------------------------------
+      bool in_mandel;
+      // Tortoise and Hare approach to check if point (x, y) is in Mandelbrot set.
+      cuDoubleComplex z_hare = make_cuDoubleComplex(0, 0);
+      cuDoubleComplex z_tortoise = make_cuDoubleComplex(0, 0);
+      cuDoubleComplex c = make_cuDoubleComplex(x, y);
+      while (true)
       {
-        denom[i][j] += (double)SAMPLES_IN_BATCH;
-        numer[i][j] += (double)count_mandelbrot(rng, SAMPLES_IN_BATCH, xmin(j), width, ymin(i), height);
-        uncert[i][j] = wald_uncertainty(numer[i][j], denom[i][j]) * width * height;
+        z_hare = cuCadd(cuCmul(z_hare, z_hare), c);
+        z_hare = cuCadd(cuCmul(z_hare, z_hare), c);
+        z_tortoise = cuCadd(cuCmul(z_tortoise, z_tortoise), c);
+        if (z_hare.x == z_tortoise.x && z_hare.y == z_tortoise.y)
+        {
+          in_mandel = true;
+          break;
+        }
+        float criteria = std::pow(z_hare.x, 2) + std::pow(z_hare.y, 2);
+        if (criteria > 4.0)
+        {
+          in_mandel = false;
+          break;
+        }
       }
+      if (in_mandel)
+        out += 1.;
+      // copied code end -------------------------
+      // if (is_in_mandelbrot(x, y))
+      // {
+      //   out += 1.;
+      // }
     }
+    numer[index] += out;
+    // end copied code----------------===================
+
+    // numer[i * NUM_TILES_1D + j] += (float)count_mandelbrot(rng, SAMPLES_IN_BATCH, xmin(j), width, ymin(i), height);
+    // COPIED CODE BEGIN ===============================
+    float n = numer[index];
+    float d = denom[index];
+    auto CheckSame = [](float a, float b) -> bool
+    { return fabs(a - b) < EPSILON; };
+    if (CheckSame(n, 0.))
+    {
+      n = 1.0;
+      d++;
+    }
+    else if (CheckSame(n, d))
+    {
+      d++;
+    }
+
+    float frac = n / d;
+    uncert[index] = sqrt(frac * (1.0 - frac) / d);
+    // COPIED CODE END =========================
+    // uncert[i * NUM_TILES_1D + j] = wald_uncertainty(numer[i * NUM_TILES_1D + j], denom[i * NUM_TILES_1D + j]) * width * height;
+    printf("(%i; %i) uncert %f id: %i\n", i, j, uncert[index], blockIdx.x * blockDim.x + threadIdx.x);
+  }
+  // }
+}
+
+__global__ void setup_kernel(curandState *state)
+{
+  int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  int seed = 1234;
+  curand_init(seed, idx, 0, &state[idx]);
 }
 
 int main(int argc, char *argv[])
@@ -126,44 +203,75 @@ int main(int argc, char *argv[])
   // }
 
   // Initialization
-  vector<vector<double>> numer(NUM_TILES_1D, vector<double>(NUM_TILES_1D));
-  vector<vector<double>> denom(NUM_TILES_1D, vector<double>(NUM_TILES_1D));
-  vector<vector<double>> uncert(NUM_TILES_1D, vector<double>(NUM_TILES_1D));
+  // Kernel Definitions
+  const int n_threads = NUM_TILES_1D; // atoi(argv[argc - 1]);
+  const int n_blocks = NUM_TILES_1D;  // atoi(argv[argc - 2]);
+
+  const int size_prefactor = n_threads * n_threads;
+  float *numer_host = new float[size_prefactor];
+  float *denom_host = new float[size_prefactor];
+  float *uncert_host = new float[size_prefactor];
+
+  float *numer_device;
+  float *denom_device;
+  float *uncert_device;
+  cudaMalloc((void **)&numer_device, size_prefactor * sizeof(float));
+  cudaMalloc((void **)&denom_device, size_prefactor * sizeof(float));
+  cudaMalloc((void **)&uncert_device, size_prefactor * sizeof(float));
 
   for (int i = 0; i < NUM_TILES_1D; i++)
     for (int j = 0; j < NUM_TILES_1D; j++)
     {
-      numer[i][j] = 0.;
-      denom[i][j] = 0.;
-      uncert[i][j] = 0.;
+      numer_host[i * NUM_TILES_1D + j] = 0.;
+      denom_host[i * NUM_TILES_1D + j] = 0.;
+      uncert_host[i * NUM_TILES_1D + j] = 0.;
     }
+
+  cudaMemcpy(numer_device, numer_host, size_prefactor * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(denom_device, denom_host, size_prefactor * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(uncert_device, uncert_host, size_prefactor * sizeof(float), cudaMemcpyHostToDevice);
 
   // random number generators & seeding
-  vector<mt19937> rngs(NUM_TILES_1D * NUM_TILES_1D);
-  for (int i = 0; i < NUM_TILES_1D * NUM_TILES_1D; i++)
-    rngs[i].seed(i);
+  curandState *d_state;
+  cudaMalloc((void **)&d_state, size_prefactor * sizeof(curandState));
+  // mt19937 rngs_host[size_prefactor];
+  // mt19937 rngs_device[NUM_TILES_1D * NUM_TILES_1D];
+  // cudaMalloc((void **)&rngs_device, size_prefactor * sizeof(mt19937));
 
-  // DO IT!
-  compute_until(rngs, numer, denom, uncert, 1e-2);
+  // for (int i = 0; i < NUM_TILES_1D * NUM_TILES_1D; i++)
+  //   rngs_host[i].seed(i);
+  // cudaMemcpy(&rngs_device, &rngs_host, size_prefactor * sizeof(mt19937), cudaMemcpyHostToDevice);
+  setup_kernel<<<n_blocks, n_threads>>>(d_state);
+
+  dim3 grid_dim(n_blocks);
+  dim3 block_dim(n_threads);
+
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+  // // DO IT ON GPU!
+  compute_until<<<grid_dim, block_dim>>>(numer_device, denom_device, uncert_device, 0.001, d_state);
+  // compute_until<<<grid_dim, block_dim>>>(rngs_device, numer_device, denom_device, uncert_device, 1e-2, d_state);
+  cudaDeviceSynchronize();
+
+  end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+
+  cudaMemcpy(numer_host, numer_device, size_prefactor * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(denom_host, denom_device, size_prefactor * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(uncert_host, uncert_device, size_prefactor * sizeof(float), cudaMemcpyDeviceToHost);
+  printf("hello");
+  // // DO IT ON CPU!
+  // compute_until(rngs_host, numer_host, denom_host, uncert_host, 1e-2);
 
   // getting the result
-  double final_value = 0;
+  float final_value = 0;
   for (int i = 0; i < NUM_TILES_1D; i++)
     for (int j = 0; j < NUM_TILES_1D; j++)
     {
-      final_value += (numer[i][j] / denom[i][j]) * width * height;
+      printf("\ndenom_host %f", denom_host[i * NUM_TILES_1D + j]);
+      final_value += (numer_host[i * NUM_TILES_1D + j] / denom_host[i * NUM_TILES_1D + j]) * width * height;
     }
-  printf("final value %lf", final_value);
-
-  // const int n_blocks = 1;  // atoi(argv[argc - 2]);
-  // const int n_threads = 1; // atoi(argv[argc - 1]);
-
-  // dim3 grid_dim(n_blocks);
-  // dim3 block_dim(n_threads);
-
-  // hello_world_gpu<<<grid_dim, block_dim>>>();
-
-  // cudaDeviceSynchronize();
-
+  printf("final value %lf\n", final_value);
+  printf("Kernel duration: %f s\n", elapsed_seconds.count());
   return 0;
 }
